4e39e5fe900201ea787fa7fc8d35a70cbf511821
unknown <lorenzo.cogliati@WIN81-Z8MFC7XR.corp.avanade.org>
2015-06-14 10:00:45 +0200

    Home layout CSS and HTML. WIP

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,39 @@
+var getHeight = function()
+{
+    var y = 0;
+    if (self.innerHeight)
+    {
+        y = self.innerHeight;
+    }
+    else if (document.documentElement && document.documentElement.clientHeight)
+    {
+        y = document.documentElement.clientHeight;
+    }
+    else if (document.body)
+    {
+        y = document.body.clientHeight;
+    }
+    return y;
+}
+
+var resizeSections = function(){
+	var sections = $("section");
+	var headerHeight = $(header).height();
+	var footerHeight = $(footer).height();
+	var documentHeight = getHeight();
+	
+	var standardH = documentHeight-headerHeight;
+	var lastElementH = standardH-footerHeight;
+
+	sections.each(function(index,elem){
+		if (index == sections.length-1){
+			elem.style.height = lastElementH+"px";
+		} else	{
+			elem.style.height = standardH+"px";
+		}
+	});
+};
+
 function loaded() {
 	console.log("Welcome!");
 
@@ -8,4 +44,5 @@ function loaded() {
 		
 	} 
 	
+	resizeSections();
 }
